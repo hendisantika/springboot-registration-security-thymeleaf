0d4608180869d7fab0629b9554556b6b09f99960
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-20 06:07:35 +0700

    #17 - Add showRegistrationForm API & registerUserAccount API in UserRegistrationController

--- src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/controller/UserRegistrationController.java
+++ src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/controller/UserRegistrationController.java
@@ -3,7 +3,9 @@ package com.hendisantika.springbootregistrationsecuritythymeleaf.controller;
 import com.hendisantika.springbootregistrationsecuritythymeleaf.dto.UserRegistrationDto;
 import com.hendisantika.springbootregistrationsecuritythymeleaf.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -29,5 +31,16 @@ public class UserRegistrationController {
     @ModelAttribute("user")
     public UserRegistrationDto userRegistrationDto() {
         return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
     }
 }
