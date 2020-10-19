399076ceea6b443c6c87e741e72a772b317c6303
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-20 06:03:35 +0700

    #9 - Add UserServiceImpl

---
+++ src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootregistrationsecuritythymeleaf.service;
+
+import com.hendisantika.springbootregistrationsecuritythymeleaf.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-registration-security-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/10/20
+ * Time: 06.03
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+}
