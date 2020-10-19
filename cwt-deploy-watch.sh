9244cab9b00d5f8371083f5729a58f790f2d54d1
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-20 06:58:10 +0700

    #27 - Add app.service as Systemd file

--- cwt-deploy-watch.sh
+++ cwt-deploy-watch.sh
@@ -2,8 +2,8 @@
 
 /usr/bin/inotifywait -m -e close_write /var/github-release |
   while read dir action file; do
-    if [[ "$file" == "cwt-spring-SNAPSHOT.jar" ]]; then
-      echo "cwt-spring-SNAPSHOT.jar received"
+    if [[ "$file" == "springboot-registration-security-thymeleaf-0.0.1.jar" ]]; then
+      echo "springboot-registration-security-thymeleaf-0.0.1.jar received"
       systemctl stop cwt.service
       mv -v "/var/github-release/${file}" /var/cwt/cwt.jar
       systemctl daemon-reload
