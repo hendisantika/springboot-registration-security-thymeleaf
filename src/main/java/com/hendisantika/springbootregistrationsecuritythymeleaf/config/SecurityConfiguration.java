e2e26af4f0e691e889596398e498319cacfa002c
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-20 06:05:39 +0700

    #13 - Add passwordEncoder & authenticationProvider bean in SecurityConfiguration

--- src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/config/SecurityConfiguration.java
@@ -2,9 +2,12 @@ package com.hendisantika.springbootregistrationsecuritythymeleaf.config;
 
 import com.hendisantika.springbootregistrationsecuritythymeleaf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +25,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
 }
