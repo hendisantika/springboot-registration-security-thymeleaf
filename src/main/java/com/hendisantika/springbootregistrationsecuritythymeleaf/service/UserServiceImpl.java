f0333fb218c48c622400777e4ef05671b820a001
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-20 06:04:04 +0700

    #10 - Add save method in UserServiceImpl

--- src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/service/UserServiceImpl.java
+++ src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootregistrationsecuritythymeleaf.service;
 
+import com.hendisantika.springbootregistrationsecuritythymeleaf.dto.UserRegistrationDto;
+import com.hendisantika.springbootregistrationsecuritythymeleaf.entity.User;
 import com.hendisantika.springbootregistrationsecuritythymeleaf.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +29,14 @@ public class UserServiceImpl implements UserService {
     public UserServiceImpl(UserRepository userRepository) {
         super();
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(UserRegistrationDto registrationDto) {
+        User user = new User(registrationDto.getFirstName(),
+                registrationDto.getLastName(), registrationDto.getEmail(),
+                passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
+
+        return userRepository.save(user);
     }
 }
