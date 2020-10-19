da6686612456c217abec3dfed3885da97a7c5405
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-20 06:06:41 +0700

    #15 - Add MainController

---
+++ src/main/java/com/hendisantika/springbootregistrationsecuritythymeleaf/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootregistrationsecuritythymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-registration-security-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/10/20
+ * Time: 06.06
+ */
+@Controller
+public class MainController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
